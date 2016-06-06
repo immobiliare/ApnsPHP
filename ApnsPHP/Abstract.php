e8b163271286e3c08ce1f391a2763ba53a4579a0
Aldo Armiento <aldo@immobiliare.it>
2016-06-06 12:12:52 +0200

    Define CURL_HTTP_VERSION_2_0 if not defined

--- ApnsPHP/Abstract.php
+++ ApnsPHP/Abstract.php
@@ -408,6 +408,10 @@ abstract class ApnsPHP_Abstract
 			);
 		}
 
+		if (!defined('CURL_HTTP_VERSION_2_0')) {
+			define('CURL_HTTP_VERSION_2_0', 3);
+		}
+
 		if (!curl_setopt_array($this->_hSocket, array(
 			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
 			CURLOPT_SSLCERT => $this->_sProviderCertificateFile,
